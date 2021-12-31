a67ce41d91d1ce28bd9383489e8f62a119c80a2c
SreenivasForLeafcraft <sreenivas@leafcraftstudios.com>
2022-01-01 02:03:50 +0530

    eedo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
 
-import { Chakra } from "./chakra";
+import { Chakra } from "../src/chakra";
+import theme from "../src/Theme";
+import { Fonts } from "../src/Theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Box maxW="900px" mx="auto">
       <Chakra cookies="cookies">
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
+          <Fonts />
           <Component {...pageProps} />
         </ChakraProvider>
       </Chakra>
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="./Fonts/Monolisa-Bold.otf" rel="stylesheet" />
+          <link href="./Fonts/Monolisa-Regular.otf" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
