4b31fd3c9397ad35b7c97853c69d620328ee499a
SreenivasForLeafcraft <sreenivas@leafcraftstudios.com>
2022-01-02 02:53:39 +0530

    Added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,14 @@ import Fonts from "../styles/Theme/fonts";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Box
+        style={{
+          background: "linear-gradient(90deg, #FC466B -2.22%, #3F5EFB 99.02%)",
+          width: "100%",
+          height: "0.5rem",
+        }}
+        marginBottom="3vh"
+      />
       <Box maxW="900px" mx="auto">
         <Chakra cookies="cookies">
           <ChakraProvider theme={theme}>
