40bad4701b8cdf7a6db6603635e25e62ed5f709c
SreenivasForLeafcraft <sreenivas@leafcraftstudios.com>
2022-01-07 01:11:28 +0530

    Edo Start CHesa

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,21 @@ import { Button, Heading, Box, Center } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import CustomButton from "../src/components/Button";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Box height="100vh">
+      <Box>
         <Heading>
           Mechatronics engineer Turned into Software Product designer, creating
           thoughtful, intutive interfaces.
         </Heading>
       </Box>
-      <Center>
-        {" "}
-        <Heading>
-          Mechatronics engineer Turned into Software Product designer, creating
-          thoughtful, intutive interfaces.
-        </Heading>
-      </Center>
 
       <Button>Hello World</Button>
+      <CustomButton variant="primary">Hello</CustomButton>
     </div>
   );
 }
