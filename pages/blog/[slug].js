7a10af33721c13c6a8bb97c4e8c71e6cc53e7982
Sreenivas Sonthena <ssaisreenivas@gmail.com>
2026-01-31 05:00:10 +0530

    style: refine blog typography and vertical rhythm

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -14,12 +14,12 @@ import { getBlogSlugs, getBlogPostBySlug } from "../../src/lib/blog";
 import { CustomHeading, CustomText } from "../../src/components/CustomComponents";
 
 const components = {
-  h1: (props) => <CustomHeading as="h1" size="2xl" my={6} {...props} />,
-  h2: (props) => <CustomHeading as="h2" size="xl" mt={10} mb={4} {...props} />,
-  h3: (props) => <CustomHeading as="h3" size="lg" mt={8} mb={4} {...props} />,
-  p: (props) => <CustomText my={6} fontSize="lg" lineHeight="1.8" color="whiteAlpha.800" {...props} />,
-  ul: (props) => <Box as="ul" ml={6} my={6} {...props} />,
-  li: (props) => <Box as="li" my={3} fontSize="lg" color="whiteAlpha.800" {...props} />,
+  h1: (props) => <CustomHeading as="h1" fontSize={{ base: "3xl", md: "4xl" }} mt={12} mb={6} color="white" {...props} />,
+  h2: (props) => <CustomHeading as="h2" fontSize={{ base: "2xl", md: "3xl" }} mt={12} mb={4} color="white" {...props} />,
+  h3: (props) => <CustomHeading as="h3" fontSize={{ base: "xl", md: "2xl" }} mt={10} mb={4} color="white" {...props} />,
+  p: (props) => <Text my={6} fontSize="lg" lineHeight="1.8" color="ash" fontFamily="MonolisaRegular" {...props} />,
+  ul: (props) => <Box as="ul" ml={6} my={6} color="ash" {...props} />,
+  li: (props) => <Box as="li" my={3} fontSize="lg" lineHeight="1.6" fontFamily="MonolisaRegular" {...props} />,
   code: (props) => (
     <Box
       as="code"
@@ -29,6 +29,7 @@ const components = {
       borderRadius="md"
       fontSize="0.9em"
       color="brand.50"
+      fontFamily="MonolisaRegular"
       {...props}
     />
   ),
@@ -49,19 +50,20 @@ export default function Post({ source, frontmatter }) {
       </Head>
 
       <Container maxW="3xl" pt={{ base: 20, md: 32 }} pb={20}>
-        <VStack align="start" spacing={8}>
-          <VStack align="start" spacing={3}>
+        <VStack align="start" spacing={12}>
+          <VStack align="start" spacing={4}>
             <Text 
               fontWeight="bold" 
-              fontSize="sm" 
-              letterSpacing="widest" 
+              fontSize="xs" 
+              letterSpacing="3px" 
               bgGradient="linear(to-r, brand.50, brand.100)"
               bgClip="text"
               textTransform="uppercase"
+              fontFamily="MonolisaBold"
             >
               Published {dateStr}
             </Text>
-            <CustomHeading fontSize={{ base: "4xl", md: "6xl" }} lineHeight="1.1">
+            <CustomHeading fontSize={{ base: "4xl", md: "6xl" }} lineHeight="1.1" letterSpacing="-1px">
               {frontmatter.title}
             </CustomHeading>
           </VStack>
@@ -74,12 +76,14 @@ export default function Post({ source, frontmatter }) {
 
           <Box pt={20} w="full">
             <Divider borderColor="whiteAlpha.100" mb={10} />
-            <VStack align="start" spacing={4}>
-              <Text fontWeight="bold" color="ash">Written by</Text>
-              <CustomHeading size="md">Sreenivas Sonthena</CustomHeading>
-              <CustomText fontSize="sm">
-                Tech Lead at Intripid building the future of travel platforms.
-              </CustomText>
+            <VStack align="start" spacing={6}>
+              <Text fontWeight="bold" color="ash" fontSize="xs" letterSpacing="2px" textTransform="uppercase">Written by</Text>
+              <VStack align="start" spacing={1}>
+                <CustomHeading fontSize="3xl">Sreenivas Sonthena</CustomHeading>
+                <CustomText fontSize="md">
+                  Tech Lead at Intripid building the future of travel platforms.
+                </CustomText>
+              </VStack>
             </VStack>
           </Box>
         </VStack>
